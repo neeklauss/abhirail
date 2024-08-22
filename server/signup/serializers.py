35f98983fff90e8993c28ae63812d3bd224ad572
abhinesh-pr <abhineshramasamy@gmail.com>
2024-08-23 00:58:41 +0530

    Simple Signup

--- server/signup/serializers.py
+++ server/signup/serializers.py
@@ -1,6 +1,6 @@
 # signup/serializers.py
 
-from rest_framework import serializers
+# from rest_framework import serializers
 from rest_framework_mongoengine.serializers import DocumentSerializer
 from .models import User
 from django.contrib.auth.hashers import make_password, check_password
@@ -13,24 +13,24 @@ class UserSerializer(DocumentSerializer):
     def validate_password(self, value):
         return make_password(value)
 
-class LoginSerializer(serializers.Serializer):
-    email = serializers.EmailField()
-    password = serializers.CharField(write_only=True)
+# class LoginSerializer(serializers.Serializer):
+#     email = serializers.EmailField()
+#     password = serializers.CharField(write_only=True)
 
-    def validate(self, data):
-        email = data.get('email')
-        password = data.get('password')
+#     def validate(self, data):
+#         email = data.get('email')
+#         password = data.get('password')
 
-        # Try to find the user with the given email
-        try:
-            user = User.objects.get(email=email)
-        except User.DoesNotExist:
-            raise serializers.ValidationError("Invalid email or password.")
+#         # Try to find the user with the given email
+#         try:
+#             user = User.objects.get(email=email)
+#         except User.DoesNotExist:
+#             raise serializers.ValidationError("Invalid email or password.")
 
-        # Manually check if the password matches
-        if not check_password(password, user.password):
-            raise serializers.ValidationError("Invalid email or password.")
+#         # Manually check if the password matches
+#         if not check_password(password, user.password):
+#             raise serializers.ValidationError("Invalid email or password.")
 
-        # Optionally, return user data in validated data
-        data['user'] = user
-        return data
+#         # Optionally, return user data in validated data
+#         data['user'] = user
+#         return data
--- server/signup/views.py
+++ server/signup/views.py
@@ -5,7 +5,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import User
 from .serializers import UserSerializer
-from .serializers import LoginSerializer
+# from .serializers import LoginSerializer
 
 @api_view(['POST'])
 def register_user(request):
@@ -15,10 +15,10 @@ def register_user(request):
         return Response({"message": "User registered successfully"}, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['POST'])
-def login_user(request):
-    serializer = LoginSerializer(data=request.data)
-    if serializer.is_valid():
-        # Optionally, you can return a token or other user data here
-        return Response({"message": "Login successful"}, status=status.HTTP_200_OK)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+# @api_view(['POST'])
+# def login_user(request):
+#     serializer = LoginSerializer(data=request.data)
+#     if serializer.is_valid():
+#         # Optionally, you can return a token or other user data here
+#         return Response({"message": "Login successful"}, status=status.HTTP_200_OK)
+#     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
